10472b22484ded809f477bed17a1b55fb8ad18cc
Carter LaVigne <143132537+spiderCSman@users.noreply.github.com>
2025-10-19 07:39:12 -0500

    Revert "Reese branch"

--- src/pages/Stocks.jsx
+++
@@ -1,8 +0,0 @@
-export default function Stocks() {
-  return (
-    <div className="flex flex-col items-center justify-center py-24 text-slate-300">
-      <h2 className="text-2xl font-bold mb-2 text-blue-300">Stocks Overview</h2>
-      <p>Monitor your investments and cosmic markets.</p>
-    </div>
-  );
-}
